9adcbbdb3b84ec3c5048e0ec2f14c93327a62541
KuchumovaAnna <ap199173@gmail.com>
2022-01-17 14:55:26 +0300

    55 and 56

---
+++ 55_001/Program.cs
@@ -0,0 +1,16 @@
+﻿// 
+
+int[,] CreateIntegerArray(int rowsNumber, int colsNumber)
+{
+    int[,] arrayToReturn = new int[rowsNumber, colsNumber];
+    for (int row = 0; row < rowsNumber; row++)
+    {
+        for (int col = 0; col < colsNumber; col++)
+        {
+            arrayToReturn[row,col] = new Random().Next(-row, row);
+        }
+    }
+    return arrayToReturn;
+}
+
+
---
+++ 56_001/Program.cs
@@ -0,0 +1,43 @@
+﻿// Написать программу, которая обменивает элементы первой строки и последней строки
+
+int[,] CreateIntegerArray(int rowsNumber, int colsNumber)
+{
+    int[,] arrayToReturn = new int[rowsNumber, colsNumber];
+    for (int row = 0; row < rowsNumber; row++)
+    {
+        for (int col = 0; col < colsNumber; col++)
+        {
+            arrayToReturn[row, col] = new Random().Next(-row, row);
+        }
+    }
+    return arrayToReturn;
+}
+
+void ExchangeRow(int[,] ArrayToExchange)
+{
+    for (int row = 0; row < CreateIntegerArray.GetLength(1); row++)
+    {
+        int buffer = ArrayToExchange[0, row];
+        ArrayToExchange[0, row] = ArrayToExchange[ArrayToExchange.GetLength(0) - 1, row];
+        ArrayToExchange[ArrayToExchange.GetLength(0) - 1, row] = buffer;
+    }
+}
+
+void Show2DArray(int[,] arrayToShow, string nameOfArray)
+{
+    Console.WriteLine($"vivod masiva {nameOfArray}");
+    for (int row = 0; row < arrayToShow.GetLength(0); row++)
+    {
+        for (int col = 0; col < arrayToShow.GetLength(0); col++)
+        {
+            Console.Write($"[{row}][{col}]: {arrayToShow[row, col]} \t");
+        }
+        Console.WriteLine();
+    }
+    System.Console.WriteLine();
+}
+
+int[,] TestArray = CreateIntegerArray(4, 5);
+Show2DArray(TestArray, "Orogin");
+ExchangeRow(TestArray);
+Show2DArray(TestArray, "Changed");
